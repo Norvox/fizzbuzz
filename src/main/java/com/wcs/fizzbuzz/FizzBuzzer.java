a8b13817006efa7dc1d8af21682b329a39a20226
Nagy-Kovács Máté <nkm.norman.89@gmail.com>
2015-03-03 01:10:39 +0100

    megoldas 001

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,7 +3,11 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if ( number % 3 == 0 && number % 5 == 0 )   return "FizzBuzz";
+        else if ( number % 3 == 0 )                 return "Fizz";
+        else if ( number % 5 == 0 )                 return "Buzz";
+        else                                        return Integer.toString( number );
     }
     
 }
